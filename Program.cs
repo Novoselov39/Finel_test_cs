b4785212973a59832ed4d921af98400e0f59f745
Anatolii <novoselov39@gmail.com>
2023-09-22 18:45:44 +0500

    Написан код

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+
+string[] mas = new string[]{"Hello", "2", "world", ":-)"};
+
+for (int i = 0; i < mas.Length; i++)
+{
+    if (mas[i].Length<=3)
+    {
+        Console.Write(mas[i]+"; ");
+    }
+    
+}
+
+
